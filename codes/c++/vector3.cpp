16026fd4d14cf80feea6488d902fbbe357bf1e87
azwad <azwadfawadhasan@gmail.com>
2022-11-02 00:02:52 +0600

    added sort colors as vector 3

---
+++ codes/c++/vector3.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+        vector<int> v1;
+        int v1Counter=0;
+        
+        for(int i =0; i<nums.size(); ++i){
+        if(nums[i]==0){
+            v1[v1Counter]=nums[i];
+            ++v1Counter;
+        }
+        else if(nums[i]==1){
+            v1[v1Counter]=nums[i];
+            ++v1Counter;
+        }
+        else{
+            v1[v1Counter]=nums[i];
+            ++v1Counter;
+        }
+    }
+    v1Counter=0;
+        
+    for(int i=0;i<v1.size();++i){
+        nums[v1Counter]=v1[i];
+        ++v1Counter;
+    }
+        
+    
+        
+
+
+}
+   
