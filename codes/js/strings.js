3916981687ed35ab4df23031ea8bd058eabac3d4
azwad <azwadfawadhasan@gmail.com>
2022-12-27 00:29:43 +0600

    added string literal and interpolation

---
+++ codes/js/strings.js
@@ -0,0 +1,12 @@
+let myString = "hello World";
+console.log(myString);
+
+//Template literals
+let boy1='nigga1';//not template literal
+let boy2='cracker1';//not template literal
+
+let sentence =`string template literal`;
+
+//STRING INTERPOLATION->when we directly insert variables in string
+sentence =`${boy1} string template literal`;
+console.log(sentence);
