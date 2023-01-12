8deb7e95d335cac9dabc5622b112c17cdde1f6c8
azwad <azwadfawadhasan@gmail.com>
2023-01-12 16:45:55 +0600

    VerifyinganAlienDictionary.js updated

--- codes/js/VerifyinganAlienDictionary.js
+++ codes/js/VerifyinganAlienDictionary.js
@@ -4,60 +4,75 @@
  * @return {boolean}
  */
 var isAlienSorted = function(words, order) {
-    var orderPointArray  = [];
-    for(var i=order.length-1 ; i>=-0;i--){
-        orderPointArray.push(order[i]);
-    }
-    var points=[];
-    var score=0;
-    for(var i=0; i<words.length-1; ++i){
-        var currWord=words[i];
-        var nextWord=words[i+1];
-        var maxLength=0;
-        var currOrnext=0;//0 means both equal//1 means curr bigger//2 means next bigger
-        if(currWord.length==nextWord.length){
-            maxLength=currWord.length
-        }else if(currWord.length>nextWord.length){
-            maxLength=currWord.length
-            currOrNext=1;
-            
-
-        }else{
-            
-            maxLength=nextWord.length
-            currOrNext =2;
-        }
-        var j=0;
-        while(j!=maxLength){
-            if(currWord[j]==null && nextWord[j]!=null){break}
-            var currLetter = currWord[j]
-            if(nextWord[j]==null && currWord[j]!=null){return false;}
-            var nextLetter = nextWord[j]
-            if(currLetter ==nextLetter){++j}
-            else{
-                //if(nextLetter!=null || currLetter!)
-                var point=0;
-                for(var k=0; k<orderPointArray.length;++k){
-                    if(currLetter == orderPointArray[k]){
-                        point=k;
-                    }
-                    else if(nextLetter == orderPointArray[k]){
-                        score=k;
-                    }
-                    if(score>point){
-                        return false;
-                    }
-                    else if(score<point || score==point){
-                            
-
-                    }
-                }
+    for(let i=1;i<words.length;i++) {
+        const a = words[i-1]
+        const b = words[i]
+        const maxLen = Math.max(a.length, b.length)
+        for(let j=0;j<maxLen;j++) {
+            const indexA = order.indexOf(a[j])
+            const indexB = order.indexOf(b[j])
+            if (indexA > indexB) {
+                return false
+            } else if (indexA < indexB) {
+                j=maxLen
             }
-
         }
-
-
-    }return true;
+    }
+    return true
+    //var orderPointArray  = [];
+    //for(var i=order.length-1 ; i>=-0;i--){
+    //    orderPointArray.push(order[i]);
+    //}
+    //var points=[];
+    //var score=0;
+    //for(var i=0; i<words.length-1; ++i){
+    //    var currWord=words[i];
+    //    var nextWord=words[i+1];
+    //    var maxLength=0;
+    //    var currOrnext=0;//0 means both equal//1 means curr bigger//2 means next bigger
+    //    if(currWord.length==nextWord.length){
+    //        maxLength=currWord.length
+    //    }else if(currWord.length>nextWord.length){
+    //        maxLength=currWord.length
+    //        currOrNext=1;
+    //        
+//
+    //    }else{
+    //        
+    //        maxLength=nextWord.length
+    //        currOrNext =2;
+    //    }
+    //    var j=0;
+    //    while(j!=maxLength){
+    //        if(currWord[j]==null && nextWord[j]!=null){break}
+    //        var currLetter = currWord[j]
+    //        if(nextWord[j]==null && currWord[j]!=null){return false;}
+    //        var nextLetter = nextWord[j]
+    //        if(currLetter ==nextLetter){++j}
+    //        else{
+    //            //if(nextLetter!=null || currLetter!)
+    //            var point=0;
+    //            for(var k=0; k<orderPointArray.length;++k){
+    //                if(currLetter == orderPointArray[k]){
+    //                    point=k;
+    //                }
+    //                else if(nextLetter == orderPointArray[k]){
+    //                    score=k;
+    //                }
+    //                if(score>point){
+    //                    return false;
+    //                }
+    //                else if(score<point || score==point){
+    //                        
+//
+    //                }
+    //            }
+    //        }
+//
+    //    }
+//
+//
+    //}return true;
   
 
 };
