00e4c85dccd5284a6488140e1e739d0fee8968fa
AzwadFawadHasan2020222 <106537814+AzwadFawadHasan2020222@users.noreply.github.com>
2023-01-31 01:31:11 +0600

    added maps.js

---
+++ codes/js/maps.js
@@ -0,0 +1,37 @@
+//we can use any type of key or value
+
+
+const myMap = new Map()
+const key1 = 'myStr'
+const key2 = {}
+const key3 = function(){}
+var myArr = new Array()
+const key4 = myArr
+
+myMap.set(key1, 'this is a string')
+myMap.set(key2, 'this is a A blank object')
+myMap.set(key3, 'this is an empty function')
+myMap.set(key4, myArr.push(8))
+
+//console.log('Maps in javascript', myMap)
+
+
+//getting the values from a Map
+let value1 = myMap.get(key1)
+console.log('value of key1 ', myMap.get(key1))
+
+//getting the size of a map
+//console.log('size of myMAP  ', myMap.size)
+
+
+//usign loops to get the value of a map
+for(let[key,value] of myMap)
+{
+    // /console.log(key,value)
+}
+
+//getting only the keys
+for(let[key] of myMap)
+{
+    console.log(key)
+}
