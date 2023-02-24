14fdc5849610df4a071a1b80dcb9b7977e70a3c1
AzwadFawadHasan2020222 <azwadfawadhasan@gmail.com>
2023-02-24 13:06:56 +0600

    added ipo.js

--- codes/js/ipo.js
+++ codes/js/ipo.js
@@ -16,7 +16,7 @@ At Most K profits
         capital[i] is needed to start it
     
         After finishing w+=pureProfit
- */
+ *//*
         var findMaximizedCapital = function(k, w, profits, capital) {
 
             var maxProjectLimit =k
@@ -33,4 +33,96 @@ At Most K profits
                 }
             }
             
+        };*/
+
+
+/**
+ * @param {number} k
+ * @param {number} w
+ * @param {number[]} profits
+ * @param {number[]} capital
+ * @return {number}
+ */
+
+/**
+At Most K profits
+
+        n projects
+
+        n[i]= pure profit
+
+        capital[i] is needed to start it
+    
+        After finishing w+=pureProfit
+ */
+        var findMaximizedCapital = function(k, w, profits, capital) {
+
+            //k no. of distict projects
+            //w iniial capital
+            var currProfit=-1
+            var currIndex=-1
+            var currCapital=-1
+            var iw=w
+            var ans=0
+            var flag=false
+            var j=0;
+            let count=0;
+            while(flag!=true){
+            for(var i=0; i < capital.length ; ++i){
+        
+                if((capital[i]<=w || capital[i]==w) && (profits[i]>currProfit)){
+                    currProfit = profits[i]
+                    currCapital=capital[i]
+                    currIndex=i
+                }
+        
+            }
+            count++;
+            j+=profits[currIndex]
+            w=w-capital[currIndex]+profits[currIndex]
+            if(count==k){flag= true}
+            if(flag==true){
+                ans=iw+j
+                return ans
+            }
+        }
+            
+        
+            
+        
+           
+            
         };
+        
+        /*
+        
+         var maxProjectLimit =false
+            var initialCapital = w
+            //mapping two arrays into a 2d array
+            let foundProject=-1
+            var iSelector=0;
+            var capIsSelected=[]
+            while(maxProjectLimit!=true){
+                for(var i =0; i <capital.length; ++i){//selecting capital and profit
+                   let projectSelector=w;
+                   let capSelect=-1
+                   if(capital[i]<=projectSelector && capital[i]>=capSelect){
+                       var index=i;
+                       
+                       capSelect = capital[i]
+            
+                   }
+                   
+                }
+                w-=capSelect
+                capIsSelected.push(index)
+                initialCapital=initialCapital+profit[capIsSelected[iSelector++]]
+                if(iSelector-1 == k){
+                    return true;
+                }
+        
+            }
+            return 
+        
+        
+            */
