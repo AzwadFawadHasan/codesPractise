ff25e1d099e9d87b9af36a283066728167c370a4
AzwadFawadHasan2020222 <azwadfawadhasan@gmail.com>
2023-02-25 22:29:34 +0600

    modified ipo.js

--- codes/js/ipo.js
+++ codes/js/ipo.js
@@ -4,6 +4,14 @@
  * @param {number[]} profits
  * @param {number[]} capital
  * @return {number}
+ * 
+ * REACTO
+ * repeat
+ * Example 
+ * Approach
+ * Code
+ * Task
+ * Optimize
  */
 
 /**
@@ -126,3 +134,21 @@ At Most K profits
         
         
             */
+
+
+            /**
+             * 
+             * class Solution {
+  fun findMaximizedCapital(k: Int, w: Int, profits: IntArray, capital: IntArray): Int {
+    val indices = (0 until profits.size).sortedWith(Comparator<Int>{a, b -> capital[b] - capital[a]}).toMutableList();
+    val pq = PriorityQueue<Int> { a, b -> b - a }
+    var res = w
+    repeat(k) {
+      while (!indices.isEmpty() && res >= capital[indices.last()])
+        pq.offer(profits[indices.removeAt(indices.size - 1)])
+      res += pq.poll() ?: 0
+    }
+    return res
+  }
+}
+             */
